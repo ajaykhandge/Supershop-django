f7356147abe7cbbd995db68ed1cc356e6e270fac
Ajay Khandge <ajaykhandge88@gmail.com>
2022-08-23 22:22:55 +0530

    Postgress config

--- Supershop/settings.py
+++ Supershop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['supershop-env.eba-xpfnvwzc.us-west-2.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['supershop-env.eba-xpfnvwzc.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -88,12 +88,24 @@ WSGI_APPLICATION = 'Supershop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
